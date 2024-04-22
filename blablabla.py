c1bc53a2fdd03508a6cdf4c0b31ef0165bdeb719
Catalin Blaga <catalin449@gmail.com>
2024-04-22 22:34:35 +0100

    bla bla bla

--- blablabla.py
+++ blablabla.py
@@ -1,3 +1,12 @@
-from colorama import Fore
+# from colorama import Fore
+#
+# print(Fore.CYAN + 'This text is red in color')
 
-print(Fore.CYAN + 'This text is red in color')
+
+
+try:
+  print(x)
+except NameError as e:
+  print(f"Variable x is not defined, {e}")
+except:
+  print("Something went wrong")
