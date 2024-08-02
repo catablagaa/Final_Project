a451e4e11cf333f8870758af60a5462eb1aab1b1
Catalin Blaga <catalin449@gmail.com>
2024-08-02 18:39:05 +0100

    bla bla

--- login.py
+++ login.py
@@ -1,47 +1,52 @@
-import json
-
-
-def read_info_from_json(path="auth.json") -> dict:
-    with open(path, "r") as f:
-        users = json.loads(f.read())
-    return users
-
-
-def changepwd(user: str):
-    auth = read_info_from_json()
-    while True:
-        new_password = input("Insert new password: ")
-        repeat = input("Write your password again: ")
-        if repeat == new_password:
-            print("Password updated successfully!")
-            auth[f"{user.lower()}"]["password"] = new_password
-            with open("auth.json", "w") as f:
-                f.write(json.dumps(auth, indent=4))
-            break
-        else:
-            print("The 2 passwords don't natch. Please try again!")
+from expense_tracker import load_json, write_json
+import progress
+import pyautogui
 
 
 def login() -> str:
-    user_info = read_info_from_json()
+    user_info = load_json("auth.json")
+    config = load_json("config.json")
+    attempts = config["attempts"]
     username = input("Insert username: ")
+
+    if username == "9":
+        exit()
+
     while username.lower() not in user_info:
         username = input("User not found! Try again: ")
-        if username.lower() == "exit":
-            exit()
-    check_if_passwd_is_correct(username)
 
+    passwd = pyautogui.password(text=f'Insert password. You have {attempts} attempts.', title='Password', default='', mask='*')
+
+    while passwd != user_info[f"{username.lower()}"]["password"]:
+        attempts -= 1
+        if attempts == 0:
+            print("You have exceeded the maximum number of attempts.")
+            exit()
+        passwd = pyautogui.password(text=f'Wrong password. You have {attempts} attempts left. Try again: ', title='',
+                                    default='', mask='*')
+    progress.progress_bar("Signing in...")
     return username
 
 
-def check_if_passwd_is_correct(username: str):
-    user_info = read_info_from_json()
-    attempts = 3
-    passwd = input(f"Insert password. You have {attempts} attempts: ")
-    while user_info[username.lower()]["password"] != passwd:
-        attempts -= 1
-        if attempts == 0:
-            print("You exceeded the maximum number of attempts!")
-            exit()
+def changepwd(user: str):
+
+    auth = load_json("auth.json")
+
+    while True:
+        while True:
+            new_password = pyautogui.password(text='Insert new password: ',
+                                        title='', default='', mask='*')
+            if new_password != auth[f'{user}']['password']:
+                break
+            else:
+                print("The new password cannot be the same as the current one")
+        repeat = pyautogui.password(text='Write your password again: ',
+                                        title='', default='', mask='*')
+        if repeat == new_password:
+            progress.progress_bar("Updating password...")
+            print("Password updated successfully!")
+            auth[f"{user.lower()}"]["password"] = new_password
+            write_json(auth, "auth.json")
+            break
         else:
-            passwd = input(f"Wrong password. {attempts} attempts left. Try again: ")
+            print("The 2 passwords don't match. Please try again!")
--- main.py
+++ main.py
@@ -1,39 +1,55 @@
+import time
 import login
 import expense_tracker as trck
-
-# In terminal pentru a transforma din gui in py:
-# pyuic6 -x .\roady_gui.ui -o roady_gui.py
-
-
+import progress
 
 if __name__ == '__main__':
     while True:
-        print(f"{9 * '*'} Welcome to your expense tracker! {10 * '*'}")
+        print(f"""{9 * '*'} Welcome to your expense tracker! {10 * '*'}""")
+        print(f"""{17 * " "}To exit, press 9 \n""")
+        time.sleep(2)
         user = login.login()
+        menu = f"""
+                            Hello, {user}!
+                            Pick an option:
+                            1. Check balance
+                            2. Add income
+                            3. Add expense
+                            4. Show list of expenses
+                            5. Remove expense
+                            6. Change password
+                            7. Log out\n"""
         while True:
-            menu = f"""
-                    Hello, {user}!
-                    Pick an option:
-                    1. Check balance
-                    2. Add income
-                    3. Add expense
-                    4. Show list of expenses
-                    5. Remove expense
-                    6. Change password
-                    7. Log out\n"""
             user_pick = input(menu)
             match user_pick:
                 case "1":
+                    progress.progress_bar("Checking balance...")
                     print(f" Your current balance is €{trck.check_balance()}")
+                    time.sleep(2)
                 case "2":
                     trck.add_income()
+                    time.sleep(2)
                 case "3":
                     trck.add_expense()
+                    time.sleep(2)
                 case "4":
-                    trck.show_expenses()
+                    progress.progress_bar("Showing expenses...")
+                    expenses = trck.show_expenses()
+                    if expenses is not None:
+                        print(expenses)
+                    time.sleep(2)
                 case "5":
-                    pass
+                    progress.progress_bar("Getting expenses...")
+                    trck.remove_expense()
+                    time.sleep(2)
                 case "6":
                     login.changepwd(user)
+                    time.sleep(2)
                 case "7":
+                    progress.progress_bar("Signing out...")
+                    print("You have logged out successfully!")
+                    time.sleep(2)
                     break
+                case _:
+                    print("Please select one of the menu options")
+                    time.sleep(2)
